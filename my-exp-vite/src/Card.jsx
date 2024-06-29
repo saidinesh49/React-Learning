7f67e82de59c4ac33c52c532cb37326e3a46074e
saidinesh49 <dineshpalisetty49@gmail.com>
2024-06-29 22:10:39 +0530

    Vite

---
+++ my-exp-vite/src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <h1 className='text-red-500 font-semibold'>
+        This is card
+      </h1>
+    </div>
+  )
+}
+
+export default Card
