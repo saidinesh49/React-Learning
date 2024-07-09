5eed2c69c85e41a5f61219a483c105033889cee3
saidinesh49 <dineshpalisetty49@gmail.com>
2024-07-09 20:34:11 +0530

    Notfoundpage-in-router

--- my-router-vite/src/components/NotFound.jsx
+++ my-router-vite/src/components/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function NotFound() {
-    return <div className='flex w-full text-4xl overflow-none bg-zinc-900 text-orange-600 justify-center items-center font-bold mt-16'
+    return <div className='flex w-full text-3xl overflow-none bg-zinc-900 text-orange-600 justify-center items-center font-bold mt-16'
     style={{minHeight:'calc(100vh - 8rem)'}} 
     >404 Page Not Found</div>;
 }
